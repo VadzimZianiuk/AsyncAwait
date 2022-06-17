799c717d91c84b680298b05f3b0a3147e8e1174c
Vadzim Zianiuk <Vadzim_Zianiuk@epam.com>
2022-06-17 17:29:24 +0200

    refactoring task 2

--- AsyncAwait.Task2.CodeReviewChallenge/Models/Support/ManualAssistant.cs
+++ AsyncAwait.Task2.CodeReviewChallenge/Models/Support/ManualAssistant.cs
@@ -1,34 +1,33 @@
-﻿using System;
+﻿using CloudServices.Interfaces;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Net.Http;
-using System.Threading;
 using System.Threading.Tasks;
-using CloudServices.Interfaces;
 
 namespace AsyncAwait.Task2.CodeReviewChallenge.Models.Support;
 
 public class ManualAssistant : IAssistant
 {
     private readonly ISupportService _supportService;
+    private readonly ILogger<ManualAssistant> _logger;
 
-    public ManualAssistant(ISupportService supportService)
+    public ManualAssistant(ISupportService supportService, ILogger<ManualAssistant> logger)
     {
         _supportService = supportService ?? throw new ArgumentNullException(nameof(supportService));
+        _logger = logger;
     }
 
     public async Task<string> RequestAssistanceAsync(string requestInfo)
     {
         try
         {
-            var t = _supportService.RegisterSupportRequestAsync(requestInfo);
-            Console.WriteLine(t.Status); // this is for debugging purposes
-            Thread.Sleep(5000); // this is just to be sure that the request is registered
-            return await _supportService.GetSupportInfoAsync(requestInfo)
-                .ConfigureAwait(false);
+            await _supportService.RegisterSupportRequestAsync(requestInfo).ConfigureAwait(false);
+            return await _supportService.GetSupportInfoAsync(requestInfo).ConfigureAwait(false);
         }
         catch (HttpRequestException ex)
         {
-            return await Task.Run(async () =>
-                await Task.FromResult($"Failed to register assistance request. Please try later. {ex.Message}"));
+            _logger?.LogError(ex, "Failed to register assistance request.");
+            return $"Failed to register assistance request. Please try later. {ex.Message}";
         }
     }
 }
